ec32d3855d21c842c351d97ffa754f5d91c1dc23
Mariem <mariemloubiri66@gmail.com>
2024-12-18 20:38:27 +0100

    msg

---
+++ src/product.js
@@ -0,0 +1,10 @@
+
+const product = {
+    name: "Laptop",
+    price: "999€",
+    description: "Un ordinateur portable haute performance pour vos besoins professionnels et personnels.",
+    image: "https://example.com/laptop.jpg"
+  };
+  
+  export default product;
+  
